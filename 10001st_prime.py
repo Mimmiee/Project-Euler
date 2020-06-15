a70008ed91850c307c94f9c3fdbcbbe45cc90299
Mimmi Edholm <id18mim@cs.umu.se>
2020-06-15 13:07:08 +0200

    7th problem

---
+++ 10001st_prime.py
@@ -0,0 +1,25 @@
+'''
+Find the 10001st prime number.
+'''
+
+import math
+count = 1
+num = 2
+
+while not count == 10001:
+    num = num +1
+    p = 2
+    prime = True
+
+    while num >= p**2:
+        if num % p == 0:
+            prime = False
+            break
+        else:
+            p = p + 1
+
+    if prime:
+        count = count + 1
+        print(count, ":", num)
+
+print("100001st prime:", num)
